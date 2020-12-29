b40f90187e77e58fd53ba6877cbfcd288721f018
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2020-12-30 00:55:18 +0600

    complete

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,27 +5,45 @@ import logo from './logo.svg'
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    // console.log(linksHeight);
+    console.log(linksContainerRef.current.getBoundingClientRect());
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return(
     <nav>
       <div className="nav-center">
         <div className="nav-header">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" className="logo" />
           <button className="nav-toggle" onClick={()=> setShowLinks(!showLinks)}>
             <FaBars />
           </button>
         </div>
-          <div className={`${showLinks?'links-container show-container':'links-container'}`}>
-            <ul className="links">
+          <div className="links-container" ref={linksContainerRef}>
+            <ul className="links" ref={linksRef}>
               {links.map((link) => {
                 const {id, url, text} = link;
-                return <li kye={id}><a href={url}>{text}</a></li>
+                return(
+                  <li kye={id}><a href={url}>{text}</a></li>
+                );
               })}
             </ul>
           </div>
         <ul className="social-icons">
           {social.map((socialItem)=>{
             const {id, url, icon} = socialItem;
-            return(<li key={id}><a href={url}>{icon}</a></li>);
+            return(
+              <li key={id}><a href={url}>{icon}</a></li>
+            );
           })}
         </ul>
       </div>
